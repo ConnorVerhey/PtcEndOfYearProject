be53e799eead7c76541500b3bd13b0de38489dd7
Connor Verhey <connor.verhey@gmail.com>
2023-04-06 18:07:46 +0000

    right=green wrong=red

---
+++ src/components/answer.svelte
@@ -0,0 +1,10 @@
+<script>
+	export let answer = false;
+</script>
+
+{#if answer}
+	<p style="color: green;">This answer was correct!</p>
+{:else}
+	<p style="color: red;">this answer was incorect!</p>
+{/if}
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,50 @@
 <script>
 	import Question from "../components/question.svelte";
-import Score from "../components/score.svelte";
+    import Score from "../components/score.svelte";
 
 </script>
 <Score score={14}/>
 
-<Question number={1}
+        <Question number={1}
     answer1="First answer"
     answer2="Second answer"
     answer3="Third answer"
     answer4="All of the above"
+    answer="c"
+    
 >
-    this is my question
+    This is my question
 </Question>
+
+<Question number={2}
+answer1="First answer"
+answer2="Second answer"
+answer3="Third answer"
+answer4="All of the above"
+answer="a"
+>
+This is my question
+</Question>
+
+<Question number={3}
+answer1="First answer"
+answer2="Second answer"
+answer3="Third answer"
+answer4="All of the above"
+answer="d"
+>
+This is my question
+</Question>
+<Question number={4}
+answer1="First answer"
+answer2="Second answer"
+answer3="Third answer"
+answer4="All of the above"
+answer="d"
+>
+This is my question
+</Question>
+
+
+
+
