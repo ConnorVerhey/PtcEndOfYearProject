b6c703aa8512dfcdfa1ee2b43df687e24f1969d0
Connor Verhey <connor.verhey@gmail.com>
2023-04-13 19:15:54 +0000

    layouts week 29

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,23 +1,48 @@
+<script>
+	import Score from "../components/score.svelte";
+</script>
 <div class="background">
 
     <div class="layout">
-        <slot/>
+        <div class="layout">
+            <div class="header">
+                <Score score={0} />
+            </div>  
+            <div class="content">
+                <slot/>
+            </div>
+        </div>
     </div>
 </div>
 
 <style>
     .background {
         padding: 32px; 
-
+        display: grid; 
         position: absolute;
         inset: 0;
-
-        background-color: lightgray;
+        background-color: var(--color-purple);
     }
 
     .layout {
         width: 100%;
-        Max-width: 900px; 
+        Max-width: 1024px; 
         margin: 0 auto; 
+
+        display: grid; 
+        grid-template-rows: auto 1fr; 
+        align-items: center; 
+    }
+
+    .header,
+    .content {
+        background-color: white;
+        border: 4px solid black;
+        padding: 12px 24px;
+    }
+
+    .header {
+    display: flex;
+    justify-content: space-between;
     }
 </style>
